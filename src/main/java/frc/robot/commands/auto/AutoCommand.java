58f7d755369b13059a608f3ec96cfe542ca790c1
Nathan Laptop <njm5865@gmail.com>
2019-01-29 18:26:39 -0600

    Added AutoDrive to AutoCommand and added a rudementary sequence selection system.

--- src/main/java/frc/robot/commands/auto/AutoCommand.java
+++ src/main/java/frc/robot/commands/auto/AutoCommand.java
@@ -1,10 +1,17 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.subsystems.AutonomousDriveTrain;
 
 public class AutoCommand extends CommandGroup {
 
-    public AutoCommand() {
+    public AutoCommand(String selected) {
+        parseSelected(selected);
 
+        addSequential(new AutoDrive(new AutonomousDriveTrain(), "example"));
+    }
+
+    public String parseSelected(String selected) {
+        return "example";
     }
 }
